0828e0d6872ecaaadf346fa4e4894497488d9bb9
mannyavila <mannyavila90@gmail.com>
2023-11-08 00:01:37 -0400

    Componente resultado finalizado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,13 +1,38 @@
 import useCotizador from "../hooks/useCotizador"
+import { MARCAS, PLANES } from "../constants"
 
 function Resultado() {
 
-    const {resultado} = useCotizador()
+    const {resultado, datos} = useCotizador()
+    const {marca, plan, year} = datos
+
+    const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
+    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
 
     if (resultado === 0) return null
   return (
-    <div>
-      {resultado}
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+        <h1 className="text-gray-600 font-bold text-3xl">Resumen</h1>
+
+        <p className="my-2">
+            <span className="font-bold">Marca: </span>
+            {nombreMarca.nombre}
+        </p>
+
+        <p className="my-2">
+            <span className="font-bold">Plan: </span>
+            {nombrePlan.nombre}
+        </p>
+
+        <p className="my-2">
+            <span className="font-bold">Año del Auto: </span>
+            {year}
+        </p>
+
+        <p className="my-2 text-2xl">
+            <span className="font-bold">Cotización Total: </span>
+            {resultado}
+        </p>
     </div>
   )
 }
